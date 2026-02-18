acea878b9dfece020f141e58cceca88c134e6fdc
ogadra <61941819+ogadra@users.noreply.github.com>
2026-02-19 02:18:23 +0900

    このわたしがッ！最初のコミットを手にしたぞッ！

---
+++ README.md
@@ -0,0 +1,4 @@
+# 20260220-lambda-demo
+
+本リポジトリは、2026年2月20日にFindy株式会社にて行われた、[TECH BATON in 東京 〜今 Lamdbaどうやって使ってる？ 〜](https://findy.connpass.com/event/383708/)での登壇において、デモで扱われたコードを保存しているリポジトリです。
+
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "AWS Lambda demo development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfreePredicate = pkg:
+            builtins.elem (pkgs.lib.getName pkg) [ "terraform" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            awscli2
+            terraform
+            python3
+            zip
+          ];
+
+          shellHook = ''
+            echo "lambda-demo dev shell"
+            echo "  aws     : $(aws --version 2>&1 | head -1)"
+            echo "  terraform: $(terraform version -json 2>/dev/null | ${pkgs.jq}/bin/jq -r '.terraform_version' 2>/dev/null || terraform version | head -1)"
+          '';
+        };
+      });
+}
